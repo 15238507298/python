8368e8c1bbb49429b6c434e85761defa9cfee1ca
lius <1986267850@qq.com>
2019-07-10 22:52:41 +0800

    we

--- com.sunland.pythonbase/python01.py
+++ com.sunland.pythonbase/python01.py
@@ -1,19 +1,37 @@
 import sys
+import traceback
 
 '''
 redirect stdout to file
 '''
+
+
 def print1(name):
-    #define file
+    # define file
     f = open('log.txt','w')
-    #file redirect to stdout
+    # file redirect to stdout
     sys.stdout = f
     print(name)
-    #screen print stream to stdout
+    # screen print stream to stdout
     sys.stdout = sys.__stdout__
     print(name)
 
 
+'''
+traceback deal Exception
+'''
 
-if __name__=='__main__':
-    print1('mary')
+
+def tt():
+    for i in '123456789098765':
+        try:
+            10.0/int(i)
+        except Exception as e:
+            traceback.print_exc()
+            print(traceback.format_exc())
+            continue
+
+
+if __name__ == '__main__':
+    # print1('mary')
+    tt()
