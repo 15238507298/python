f2692b413c7428f3274b1041b9d9b4612cdbcbaa
lius <1986267850@qq.com>
2019-07-14 23:01:16 +0800

    django-urls

--- Django_urls/front/urls.py
+++ Django_urls/front/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
 
-
+#定义应用空间
 app_name = 'front'
 
 urlpatterns = [
+    #name 给路径起名字，可以通过reverse（name）获取url
     path('', views.index, name='index'),
     path('login', views.login, name='login')
 
